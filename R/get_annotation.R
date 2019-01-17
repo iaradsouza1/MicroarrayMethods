df5cc4ca42292c98ffae04b6a48cf05f704d53e5
Clovis F Reis <clovis@wofdesk.wolfzone>
2019-01-17 15:13:17 -0300

    Update get_annotation

--- R/get_annotation.R
+++ R/get_annotation.R
@@ -3,7 +3,7 @@
 #' Get annotation for a specific platform
 #'
 #' @param dataset ENSEMBL dataset name. A dataset name can be retrieved with list_datasets() function.
-#' @param platform Platform name in ENSEMBL. Usually one retrieved by list_platforms() function
+#' @param platform Platform name in ENSEMBL. Usually one retrieved by list_platforms() function. Use "none" if you aren't working with microarray. In that case the parameter "att" is mandatory.
 #' @param db One of the following ENSEMBL databases: "main" for ENSEMBL_MART_ENSEMBL database (the main database in ENSEMBL, provides information for the majority of species)
 #' and "mouse" for ENSEMBL_MART_MOUSE database (which provides specific information of mouse strains)
 #' @param att Attributes to be retrieved. If not specified, retrives HGNC symbol and ENTREZ id.
@@ -15,10 +15,20 @@
 #' @export
 #'
 #' @examples
-get_annotation <- function(dataset, platform, db = "main", att = c("hgnc_symbol", "entrezgene")) {
+get_annotation <- function(dataset, platform="none", db = "main", att ) {
+  if (missing("dataset")){
+    stop("A dataset object must be provided\n")
+  }
+  if (platform == "none"){
+    if (missing("att")){
+      stop("An vector of attributes or a plataform must be provided\n")
+    }
+  }else {
+    att = c(platform, "hgnc_symbol", "entrezgene")#replaces line att <- c(platform, att)
+  }
   if (db == "main") {
     ensembl <- biomaRt::useDataset(dataset = dataset, mart = biomaRt::useMart("ENSEMBL_MART_ENSEMBL"))
-    att <- c(platform, att)
+    #att <- c(platform, att) #replaced by line 27
     id <- getBM(attributes = att, mart = ensembl)
 
     id[id == ""] <- NA
@@ -26,7 +36,7 @@ get_annotation <- function(dataset, platform, db = "main", att = c("hgnc_symbol"
     return(id)
   } else if (db == "mouse") {
     ensembl <- biomaRt::useDataset(dataset = dataset, mart = biomaRt::useMart("ENSEMBL_MART_MOUSE"))
-    att <- c(platform, att)
+    #att <- c(platform, att) #replaced by line 27
     id <- getBM(attributes = att, mart = ensembl)
     id[id == ""] <- NA
     id <- unique(na.omit(id))
